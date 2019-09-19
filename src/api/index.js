bbaac3ac3df058b1e3f27348194af72aa81bff6a
Tolik <dzhudzhiev@gmail.com>
2019-09-19 09:48:10 +0000

    client with no payment

--- src/api/index.js
+++ src/api/index.js
@@ -1,12 +1,12 @@
 import axios from 'axios';
 
 const getTrips = (data) => {
-  return axios.post('http://157.245.67.190/:5000/api/trips', { data }).then(res => res.data )
+  return axios.post('/api/trips', { data }).then(res => res.data )
 }
 
 const sendOrder = (data) => {
   console.log('send order', data);
-  return axios.post('http://157.245.67.190/:5000/api/order', { data }).then(res => res.data )
+  return axios.post('/api/order', { data }).then(res => res.data )
 }
 
 export default {
