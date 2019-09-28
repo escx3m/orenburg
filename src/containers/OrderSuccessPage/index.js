b9647dd2457577242b565b41b29f21618b931d6c
Anatolii Dzhudzhiev <dzhudzhiev@gmail.com>
2019-09-28 18:55:01 +0300

    Инструкция после завершения оплаты

--- src/containers/OrderSuccessPage/index.js
+++ src/containers/OrderSuccessPage/index.js
@@ -4,6 +4,7 @@ import { makeStyles } from '@material-ui/core/styles';
 import Typography from '@material-ui/core/Typography';
 import Button from '@material-ui/core/Button';
 import Grid from '@material-ui/core/Grid';
+import Container from '@material-ui/core/Container';
 
 import { passangersReset } from '../PersonInfoPage/actions';
 import { tripsReset } from '../SearchPage/actions';
@@ -11,6 +12,15 @@ import { tripsReset } from '../SearchPage/actions';
 const useStyles = makeStyles(theme => ({
   root: {
     padding: theme.spacing(3, 2),
+  },
+  heroContent: {
+    padding: theme.spacing(3, 0, 6),
+  },
+  textIndent: {
+    textIndent: "1.5em"
+  },
+  heroButtons: {
+    marginTop: theme.spacing(4),
   },
 }));
 
@@ -26,18 +36,29 @@ const OrderSuccessPage = (props) => {
   const classes = useStyles();
 
   return (
-    <Grid container spacing={2} className={classes.root}>
-      <Grid item xs={12}>
-        <Typography variant="h5" component="h2" align="center">
-          Бронь успешно оформлена!
+    <div className={classes.heroContent}>
+      <Container maxWidth="sm">
+        <Typography component="h5" variant="h5" align="center" color="textPrimary" gutterBottom>
+          Спасибо, что купили у нас билет.
         </Typography>
-      </Grid>
-      <Grid item xs={12}>
-        <Grid container justify="center">
-          <Button onClick={handleClick} variant="contained" color="primary">В начало</Button>
+        <Typography className={classes.textIndent}>
+          Если нам понадобится какая-либо информация по вашей поездке, мы свяжемся с вами
+          по контактным номерам указанным в билете.
+        </Typography>
+        <Typography className={classes.textIndent}>
+          За день до поездки с 19:00 до 20:00 ожидайте смс с номером водителя и маркой машины. В указанный диапазон сбора
+          будьте готовы. За 5-10 минут до прибытия вам позвонит водитель.
+        </Typography>
+        <Typography className={classes.textIndent}>
+          Телефон диспетчерской <phone>8937464600</phone>
+        </Typography>
+        <Grid container spacing={2} justify="center" className={classes.heroButtons}>
+          <Grid item>
+            <Button onClick={handleClick} variant="contained" color="primary">Купить обратный билет</Button>
+          </Grid>
         </Grid>
-      </Grid>
-    </Grid>
+      </Container>
+    </div>
   );
 }
 
