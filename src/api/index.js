4d255f26189252a5ff47f925a80f5051f3a68cdb
escx3m <ololoataked@gmail.com>
2020-01-23 15:54:29 +0300

    1

--- src/api/index.js
+++ src/api/index.js
@@ -1,11 +1,15 @@
 import axios from "axios";
 
 const getTrips = data => {
-  return axios.post("/api/trips", { data }).then(res => res.data);
+  return axios
+    .post("http://localhost:5000/api/trips", { data })
+    .then(res => res.data);
 };
 
 const sendOrder = async data => {
-  const result = axios.post("/api/order", { data }).then(res => res);
+  const result = axios
+    .post("http://localhost:5000/api/order", { data })
+    .then(res => res);
   return result;
 };
 
